6d718f04591534bb74fd845f6997e3c16087b3d6
Emilio <emiliosneto13@gmail.com>
2023-03-10 23:39:33 -0300

    Configure migrations

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,4 +1,4 @@
-import { TypeOrmModule } from '@nestjs/typeorm/dist';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { EnvConfig } from 'src/env';
 
 export const DatabaseModule = TypeOrmModule.forRootAsync({
@@ -10,6 +10,8 @@ export const DatabaseModule = TypeOrmModule.forRootAsync({
     password: config.DB_PASSWORD,
     database: config.DB_NAME,
     entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+    migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
+    migrationsRun: true,
   }),
   inject: [EnvConfig],
 });
